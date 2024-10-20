1ef2c39e523bb51bdd5251bfd26f8adf53d17f8e
llsgllsg <151439431+llsgllsg@users.noreply.github.com>
2024-10-20 19:58:35 +0800

    Update update_desc.py

--- update_desc.py
+++ update_desc.py
@@ -1,26 +1,25 @@
 import requests
 import os
-import re
 
-# API 接口地址
+# API接口地址
 url = "https://api.leafone.cn/api/lishi?type=rand"
 
 try:
-    # 发送 GET 请求
+    # 发送GET请求
     response = requests.get(url)
 
     # 检查请求是否成功
     if response.status_code == 200:
-        # 解析返回的 JSON 数据
+        # 解析返回的JSON数据
         data = response.json()
-        print("API返回的数据:", data)  # 打印 API 返回的数据
+        print("API返回的数据:", data)  # 打印API返回的数据
 
         # 获取必要的信息
         title = data.get("data", {}).get("title", "无标题信息")
         type_event = data.get("data", {}).get("type", "无类型信息")
         desc_text = data.get("data", {}).get("desc", "无描述信息")
 
-        # 生成或修改 main.xaml 文件的路径
+        # 生成或修改main.xaml文件的路径
         file_path = "main.xaml"
         
         if not os.path.exists(file_path):
@@ -39,19 +38,70 @@ try:
 
             print("旧内容:\n", content)  # 打印旧内容
             
-            # 使用正则表达式替换第一个匹配的内容
-            pattern = r'(<local:MyCard Title="事件：)(.*?)(，类型：.*?" Margin=.*?>\n\s+<StackPanel.*?>\n\s+<TextBlock TextWrapping="Wrap" Margin="0,0,0,4" Text=")(.*?)("\s*/>\n\s+</StackPanel>\n\s+</local:MyCard>'
-            new_content = f'\\1{title}，类型：{type_event}\\3{desc_text}\\5'
-            
-            updated_content = re.sub(pattern, new_content, content, count=1, flags=re.DOTALL)
+            # 建立 expected 新内容字符串
+            new_content = f'<local:MyCard Title="事件：{title}，类型：{type_event}" Margin="0,0,0,15" CanSwap="True" IsSwaped="True">\n' \
+                          f'\t<StackPanel Margin="25,40,23,15">\n' \
+                          f'\t\t<TextBlock TextWrapping="Wrap" Margin="0,0,0,4" Text="{desc_text}" />\n' \
+                          f'\t</StackPanel>\n' \
+                          f'</local:MyCard>\n'
 
+            print("构建的新内容:\n", new_content)  # 打印新内容
+            
             # 仅当内容不同的时候才写入新的内容
-            if content != updated_content:
+            if content != new_content:
                 with open(file_path, "w", encoding='utf-8') as f:
-                    f.write(updated_content)
+                    f.write(new_content)
                 print("main.xaml 文件已更新")
             else:
                 print("内容没有变化，不进行更新。")
+
+        # 添加内容
+        with open(file_path, "a", encoding='utf-8') as f:  # 以追加模式打开文件
+            f.write('\n<local:MyCard Margin="0,-6,0,12" Title="欢迎"> <!--下面不是卡片，所以不用0,0,0,12-->\n')
+            f.write('     <StackPanel Margin="24,35,24,15">\n')
+            f.write('          <TextBlock HorizontalAlignment="Center" Margin="0,0,0,0"\n')
+            f.write('               Foreground="{DynamicResource ColorBrush2}" FontSize="20"\n')
+            f.write('               Text="欢迎使用历史上的今天主页!" />\n')
+            f.write('          <Calendar HorizontalAlignment="Center" Margin="0,12,0,10" />\n')
+            f.write('          <TextBlock Margin="5,0,5,12" TextWrapping="Wrap" HorizontalAlignment="Center"\n')
+            f.write('               Foreground="{DynamicResource ColorBrush1}" Text="{cave}" />\n')
+            f.write('     </StackPanel>\n')
+            f.write('</local:MyCard> \n\n')  # 添加结束标记和换行
+
+            # 继续追加其他内容
+            f.write('<local:MyCard Margin="0,0,0,8">\n')
+            f.write('     <StackPanel Margin="24,6,24,12">\n')
+            f.write('          <Grid>\n')
+            f.write('               <Grid.ColumnDefinitions>\n')
+            f.write('                    <ColumnDefinition Width="1*" />\n')
+            f.write('                    <ColumnDefinition Width="1*" />\n')
+            f.write('               </Grid.ColumnDefinitions>\n')
+            f.write('               <local:MyIconTextButton Margin="0,5,0,6" Height="35" HorizontalAlignment="Center"\n')
+            f.write('                    Text="作者网站" EventType="打开网页" Grid.Column="0"\n')
+            f.write('                    EventData="https://llsgllsg.rth1.xyz"\n')
+            f.write('                    LogoScale="0.8" ColorType="Highlight"\n')
+            f.write('                    Logo="M13.5,4A1.5,1.5 0 0,0 12,5.5A1.5,1.5 0 0,0 13.5,7A1.5,1.5 0 0,0 15,5.5A1.5,1.5 0 0,0 13.5,4M13.14,8.77C11.95,8.87 8.7,11.46 8.7,11.46C8.5,11.61 8.56,11.6 8.72,11.88C8.88,12.15 8.86,12.17 9.05,12.04C9.25,11.91 9.58,11.7 10.13,11.36C12.25,10 10.47,13.14 9.56,18.43C9.2,21.05 11.56,19.7 12.17,19.3C12.77,18.91 14.38,17.8 14.54,17.69C14.76,17.54 14.6,17.42 14.43,17.17C14.31,17 14.19,17.12 14.19,17.12C13.54,17.55 12.35,18.45 12.19,17.88C12,17.31 13.22,13.4 13.89,10.71C14,10.07 14.3,8.67 13.14,8.77Z" />\n')
+            f.write('               <local:MyIconTextButton Margin="0,5,0,6" Height="35" HorizontalAlignment="Center"\n')
+            f.write('                    Text="刷新主页" EventType="刷新主页" Grid.Column="1"\n')
+            f.write('                    LogoScale="0.8" ColorType="Highlight"\n')
+            f.write('                    Logo="M256.455,8C322.724,8.119,382.892,34.233,427.314,76.685L463.029,40.97C478.149,25.851,504,36.559,504,57.941L504,192C504,205.255,493.255,216,480,216L345.941,216C324.559,216,313.851,190.149,328.97,175.029L370.72,133.279C339.856,104.38 299.919,88.372 257.49,88.006 165.092,87.208 87.207,161.983 88.0059999999999,257.448 88.764,348.009 162.184,424 256,424 297.127,424 335.997,409.322 366.629,382.444 371.372,378.283 378.535,378.536 382.997,382.997L422.659,422.659C427.531,427.531 427.29,435.474 422.177,440.092 378.202,479.813 319.926,504 256,504 119.034,504 8.001,392.967 8,256.002 7.999,119.193 119.646,7.755 256.455,8z" />\n')
+            f.write('          </Grid>\n')
+            f.write('          <local:MyListItem Margin="-2,0,0,0"\n')
+            f.write('               Logo="pack://application:,,,/images/blocks/GoldBlock.png" Title="今日人品"\n')
+            f.write('               Info="试试手气！" EventType="今日人品" Type="Clickable" />\n')
+            f.write('          <local:MyListItem Margin="-2,0,0,0"\n')
+            f.write('               Logo="pack://application:,,,/images/blocks/Grass.png" Title="启动基岩版"\n')
+            f.write('               Info="以 URL Scheme 的方式启动电脑上的 MC 基岩版。" EventType="打开网页" EventData="minecraft://"\n')
+            f.write('               Type="Clickable" />\n')
+            f.write('          <local:MyListItem Margin="-2,0,0,0"\n')
+            f.write('               Logo="pack://application:,,,/images/blocks/CommandBlock.png" Title="网页捷径"\n')
+            f.write('               Type="Clickable"\n')
+            f.write('               Info="打开网页捷径页面" EventType="打开帮助"\n')
+            f.write('               EventData="https://cn-sy1.rains3.com/123456/jj.json" />\n')
+            f.write('     </StackPanel>\n')
+            f.write('</local:MyCard>\n')
+
+        print("附加内容已成功添加到 main.xaml 文件中。")
 
     else:
         print("请求失败，状态码：", response.status_code)
