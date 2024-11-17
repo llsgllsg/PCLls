df5677cbf3903174ac2ea105ce55813c359fd11d
llsgllsg <151439431+llsgllsg@users.noreply.github.com>
2024-11-17 19:54:32 +0800

    Update update_desc.py

--- update_desc.py
+++ update_desc.py
@@ -23,7 +23,7 @@ try:
         file_path = "main.xaml"
         
         # 新增的前缀内容
-        header_content = '<local:MyCard Title="欢迎使用历史上的今天" Margin="10,0,250,15" CanSwap="False" IsSwaped="True">\n    <StackPanel Margin="25,75,23,15">\n        <local:MyImage Height="200" HorizontalAlignment="Center" Source="https://cn-sy1.rains3.com/123456/tp.png" />\n    </StackPanel>\n</local:MyCard>\n'  
+        header_content = '<local:MyCard Title="欢迎使用历史上的今天" Margin="0,0,300,15" CanSwap="False" IsSwaped="True">\n    <StackPanel Margin="25,75,23,15">\n        <local:MyImage Height="200" HorizontalAlignment="Center" Source="https://cn-sy1.rains3.com/123456/tp.png" />\n    </StackPanel>\n</local:MyCard>\n'  
         if not os.path.exists(file_path):
             # 如果文件不存在，创建文件并写入基本结构
             with open(file_path, "w", encoding='utf-8') as f:
@@ -42,7 +42,7 @@ try:
             print("旧内容:\n", content)  # 打印旧内容
             
             # 建立 expected 新内容字符串
-            new_content = f'<local:MyCard Title="事件：{title}，类型：{type_event}" Margin="300,-305,0,15" CanSwap="False" IsSwaped="True">\n' \
+            new_content = f'<local:MyCard Title="事件：{title}，类型：{type_event}" Margin="250,-305,0,15" CanSwap="False" IsSwaped="True">\n' \
                           f'\t<StackPanel Margin="25,40,23,15">\n' \
                           f'\t\t<TextBlock TextWrapping="Wrap" Margin="0,0,0,4" Text="{desc_text}" />\n' \
                           f'\t</StackPanel>\n' \
